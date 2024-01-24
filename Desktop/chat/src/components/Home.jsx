5d08e82c974be5cdf4cf65c1750c60fbb7327a91
ARPAN CHOWDHURY <77331658+Arpan1908@users.noreply.github.com>
2024-01-24 15:50:52 +0530

    Update Home.jsx

--- Desktop/chat/src/components/Home.jsx
+++ Desktop/chat/src/components/Home.jsx
@@ -1,15 +1,17 @@
 import Togglecolor from './Tooglecolor';
 import Views from './Views';
+import {Grid,GridItem,Tabs} from '@chakra-ui/react'
+import Sidebar from './Home/Sidebar';
 
 
 function Home() {
   return (
-    <>
-    <Views />
-    <Togglecolor />
-
-    hii
-    </>
+    <Grid templateColumns="repeat(10,1fr)" h="100vh" as={Tabs}>
+      <GridItem colSpan="3" borderRight="solid 1px gray">
+        <Sidebar />
+      </GridItem>
+      <GridItem colSpan="7"></GridItem>
+    </Grid>
   );
 }
 
