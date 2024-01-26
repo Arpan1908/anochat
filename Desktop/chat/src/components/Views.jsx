72fa2babd624181c02e3ca96099c7a4a51429840
ARPAN CHOWDHURY <77331658+Arpan1908@users.noreply.github.com>
2024-01-26 13:23:43 +0530

    Update Views.jsx

--- Desktop/chat/src/components/Views.jsx
+++ Desktop/chat/src/components/Views.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Login from './logins/Login'
 import Signup from './logins/Signup'
 import Home from './Home'
 import Private from './Private'
+import AContext from './Context'
+import Landing from './pages/Landing'
 const Views = () => {
-  return (
+  
+  return   (
+  
+  
+    
     <Routes>
-      <Route element={<Private />}>
+       <Route element={<Private />}>
         <Route path="/home" element={<Home />} />
       </Route>
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
     </Routes>
   )
+    
+  
 }
 
 export default Views
