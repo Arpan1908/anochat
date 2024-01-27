aecdd1d36ce30bb6f312ee10c21e5ca60e47750b
ARPAN CHOWDHURY <77331658+Arpan1908@users.noreply.github.com>
2024-01-27 13:59:41 +0530

    Update Home.jsx

--- Desktop/chat/src/components/Home.jsx
+++ Desktop/chat/src/components/Home.jsx
@@ -2,16 +2,28 @@ import Togglecolor from './Tooglecolor';
 import Views from './Views';
 import {Grid,GridItem,Tabs} from '@chakra-ui/react'
 import Sidebar from './Home/Sidebar';
+import {useState} from 'react';
+import Chat from "./Home/Chat";
+
+import { createContext } from 'react';
+
+export const FriendContext = createContext();
 
 
 function Home() {
+  const [list, setList] = useState([]);
+
   return (
-    <Grid templateColumns="repeat(10,1fr)" h="100vh" as={Tabs}>
-      <GridItem colSpan="3" borderRight="solid 1px gray">
-        <Sidebar />
-      </GridItem>
-      <GridItem colSpan="7"></GridItem>
-    </Grid>
+  <FriendContext.Provider value={{ list, setList }}>
+      <Grid templateColumns="repeat(10, 1fr)" h="100vh" as={Tabs}>
+        <GridItem colSpan="3" borderRight="1px solid gray">
+          <Sidebar />
+        </GridItem>
+        <GridItem colSpan="7">
+          <Chat />
+        </GridItem>
+      </Grid>
+    </FriendContext.Provider>
   );
 }
 
